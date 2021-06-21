4a1948f5aff3fc98d1a4667c21a31acb84650f6f
wlandau <will.landau@gmail.com>
2021-06-21 16:28:29 -0400

    Relax assertion

--- R/utils_assert.R
+++ R/utils_assert.R
@@ -1,11 +1,4 @@
 assert_stan_file <- function(stan_file) {
-  targets::tar_assert_scalar(
-    stan_file,
-    paste(
-      "stan_file must have length 1.",
-      "Some stantargets functions do allow multiple Stan files though."
-    )
-  )
   targets::tar_assert_chr(stan_file)
   targets::tar_assert_path(stan_file)
   if (dir.exists(stan_file)) {
