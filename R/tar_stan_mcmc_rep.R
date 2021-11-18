c951c996a37a44e630923454a61a61e83a5c0f27
wlandau <will.landau@gmail.com>
2021-11-17 21:03:14 -0500

    Fix URLs

--- R/tar_stan_mcmc_rep.R
+++ R/tar_stan_mcmc_rep.R
@@ -24,7 +24,7 @@
 #'   summaries, include a `.join_data`
 #'   element of your Stan data list with names and dimensions corresponding
 #'   to those of the model. For details, read
-#'   <https://docs.ropensci.org/stantargets/articles/mcmc_rep.html>.
+#'   <https://docs.ropensci.org/stantargets/articles/simulation.html>.
 #' @param batches Number of batches. Each batch is a sequence
 #'   of branch targets containing multiple reps. Each rep
 #'   generates a dataset and runs the model on it.
@@ -308,7 +308,7 @@ tar_stan_mcmc_rep <- function(
 #'   for each rep. To join more than just scalars, include a `.join_data`
 #'   element of your Stan data list with names and dimensions corresponding
 #'   to those of the model. For details, read
-#'   <https://docs.ropensci.org/stantargets/articles/mcmc_rep.html>.
+#'   <https://docs.ropensci.org/stantargets/articles/simulation.html>.
 tar_stan_mcmc_rep_run <- function(
   stan_file,
   stan_name,
--- README.md
+++ README.md
@@ -11,7 +11,7 @@ Targetopia](https://img.shields.io/badge/R_Targetopia-member-blue?style=flat&lab
 -->
 [![active](https://www.repostatus.org/badges/latest/active.svg)](https://www.repostatus.org/#active)
 [![check](https://github.com/ropensci/stantargets/workflows/check/badge.svg)](https://github.com/ropensci/stantargets/actions?query=workflow%3Acheck)
-[![codecov](https://codecov.io/gh/ropensci/stantargets/branch/main/graph/badge.svg?token=3T5DlLwUVl)](https://codecov.io/gh/ropensci/stantargets)
+[![codecov](https://codecov.io/gh/ropensci/stantargets/branch/main/graph/badge.svg?token=3T5DlLwUVl)](https://app.codecov.io/gh/ropensci/stantargets)
 [![lint](https://github.com/ropensci/stantargets/workflows/lint/badge.svg)](https://github.com/ropensci/stantargets/actions?query=workflow%3Alint)
 
 Bayesian data analysis usually incurs long runtimes and cumbersome
