bc9ef6d2c7afa6433ece45c1840779db4181be9f
wlandau-lilly <will.landau@lilly.com>
2025-02-07 12:47:14 -0500

    qs2

--- R/tar_stan_package.R
+++ R/tar_stan_package.R
@@ -16,7 +16,7 @@
 #' @importFrom cmdstanr cmdstan_model
 #' @importFrom fs path_ext_remove path_rel
 #' @importFrom fst read_fst
-#' @importFrom qs qread
+#' @importFrom qs2 qs_read
 #' @importFrom posterior as_draws_df
 #' @importFrom purrr map map_dbl map2_dfr
 #' @importFrom rlang check_installed expr quo_squash
